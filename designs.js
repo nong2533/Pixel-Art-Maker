8f8c68fdd614180eadde63439a983ecd7e7a4544
nong2533 <107129866+nong2533@users.noreply.github.com>
2022-10-20 23:40:55 +0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -12,13 +12,24 @@ size.addEventListener('submit', function (event) {
     event.preventDefault();
     makeGrid(height.value, width.value);
 } );
-
+/** Create a grid of squares 
+ * int width - number of squares representing the width of the grid
+ * int height - number of squares representing the height of the grid
+*/
 function makeGrid(height, width) {
+    /** innerHTML - reset every cell in the table
+     * when clicked is 'submit' again
+     */
     canvas.innerHTML = null;
+    /** make the grid in rows */
     for( h = 0; h < height; h++){
         let row = canvas.insertRow(h);
+        /**make the cell in colunm */
         for ( w = 0; w < width; w++){
             let cell = row.insertCell(w);
+            /** When clicked is cell by the user , 
+             * change to selected color
+             * and when clicked again reset color from cell   */
             cell.addEventListener('click', function(){
                 if(cell.style.backgroundColor) {
                     cell.style.backgroundColor = "";
